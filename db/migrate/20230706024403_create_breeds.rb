7c8fb5dbd11c9a23eef21f2a55ab6222983b3504
hoangnguyen249 <Joshbub2409@gmail.com>
2023-07-11 23:38:32 -0500

    add some variables

--- db/migrate/20230706024403_create_breeds.rb
+++ db/migrate/20230706024403_create_breeds.rb
@@ -2,6 +2,8 @@ class CreateBreeds < ActiveRecord::Migration[7.0]
   def change
     create_table :breeds do |t|
       t.string :name
+      t.string :gender
+      t.string :size
       t.references :owner, foreign_key: true
 
       t.timestamps
