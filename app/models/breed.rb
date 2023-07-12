ea4d1094f12304dcba107fdb0b457724709d7e59
hoangnguyen249 <Joshbub2409@gmail.com>
2023-07-12 00:04:18 -0500

    fixing

--- app/models/breed.rb
+++ app/models/breed.rb
@@ -1,6 +1,7 @@
 class Breed < ApplicationRecord
   has_and_belongs_to_many :owners, join_table: :breeds_owners
   has_many :images
-
+  attribute :gender, :string
+  attribute :size, :string
   validates :name, presence: true
 end
--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,5 +1,6 @@
 class Owner < ApplicationRecord
   has_and_belongs_to_many :breeds, join_table: :breeds_owners
+  attribute :address, :string
 
   validates :name, presence: true
 end
