0cad9c554cae15d39c35905f446d71eb66bb0a21
hoangnguyen249 <Joshbub2409@gmail.com>
2023-07-12 00:01:49 -0500

    fixing

--- db/migrate/20230706024403_create_breeds.rb
+++ db/migrate/20230706024403_create_breeds.rb
@@ -2,11 +2,12 @@ class CreateBreeds < ActiveRecord::Migration[7.0]
   def change
     create_table :breeds do |t|
       t.string :name
+      t.string :gender
+      t.string :size
       t.references :owner, foreign_key: true
 
       t.timestamps
-      add_column :breeds, :gender, :string
-      add_column :breeds, :size, :string
     end
   end
 end
+
